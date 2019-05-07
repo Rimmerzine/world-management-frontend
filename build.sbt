7c6e1ca8b8bda7257b392596cdf84e76975fd2a3
Rimmerzine <alexrimmer1994@gmail.com>
2019-05-07 16:12:45 +0100

    Updated HomeViewSpec so that it no longer uses Application, does not check messages conten (only checks key), refactored to use a ViewSetup Upgraded to Play 2.7.2, Upgraded to sbt 1.2.8

--- build.sbt
+++ build.sbt
@@ -1,21 +1,24 @@
 name := "WorldManagement"
  
 version := "0.1"
-      
-lazy val `worldmanagement` = (project in file(".")).enablePlugins(PlayScala)
 
-resolvers += "scalaz-bintray" at "https://dl.bintray.com/scalaz/releases"
+lazy val scalaTest: ModuleID = "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.2"
+lazy val jsoup: ModuleID = "org.jsoup" % "jsoup" % "1.11.3"
       
-resolvers += "Akka Snapshot Repository" at "http://repo.akka.io/snapshots/"
+lazy val `worldmanagement` = (project in file("."))
+  .enablePlugins(PlayScala)
+  .configs(IntegrationTest)
+  .settings(
+    Defaults.itSettings,
+    libraryDependencies ++= Seq(
+      jdbc,
+      ehcache,
+      ws,
+      specs2 % Test,
+      guice,
+      scalaTest % "it,test",
+      jsoup % "it,test"
+    )
+  )
       
 scalaVersion := "2.12.2"
-
-libraryDependencies ++= Seq( jdbc , ehcache , ws , specs2 % Test , guice )
-
-libraryDependencies ++= Seq(
-  "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % Test
-)
-
-unmanagedResourceDirectories in Test <+=  baseDirectory ( _ /"target/web/public/test" )
-
-      
--- test/views/HomeViewSpec.scala
+++ test/views/HomeViewSpec.scala
@@ -1,36 +1,31 @@
 package views
 
 import helpers.ViewSpec
-import org.scalatestplus.play.guice.GuiceOneAppPerSuite
-import play.api.i18n.{Messages, MessagesApi}
-import play.api.mvc.AnyContent
-import play.api.test.FakeRequest
-import play.test.Helpers._
 import play.twirl.api.Html
 
-class HomeViewSpec extends ViewSpec with GuiceOneAppPerSuite {
+class HomeViewSpec extends ViewSpec {
 
-  val messagesApi: MessagesApi = app.injector.instanceOf[MessagesApi]
-  val request: FakeRequest[AnyContent] = FakeRequest()
-  implicit val messages: Messages = messagesApi.preferred(request)
-  val page: Html = views.html.home()
-  val pageContent: String = contentAsString(page)
+  class Setup extends ViewSetup {
+    val page: Html = views.html.home()
+  }
 
   "The home page" must {
-    "have the correct content type" in {
+    "have the correct content type" in new Setup {
       page.contentType mustBe "text/html"
     }
 
-    "display the title correctly" in  {
-      pageContent contains titleHtml("Start creating your D&D world") mustBe true
+    "display the title correctly" in new Setup {
+      document.title mustBe "home.title"
     }
 
-    "display the header correctly" in {
-      pageContent contains headerHtml("Start creating your D&D world") mustBe true
+    "display the heading" in new Setup {
+      getTextOfElementById("heading") mustBe Some("home.header")
     }
 
-    "contain a form linked to the pages submit action" in {
-      pageContent contains formHtml("/start", "POST")
+    "contain a form linked to the pages submit action" in new Setup {
+      getElementById("home-form").isDefined mustBe true
+      getAttrOfElementById("home-form", "action") mustBe Some("/start")
+      getAttrOfElementById("home-form", "method") mustBe Some("POST")
     }
   }
 }
